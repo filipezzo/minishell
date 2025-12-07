85a14a3b797c3b91ce4c18e7b43c107629a0d2fb
Filipe Avanzzo <fastnexi@gmail.com>
2025-12-06 23:42:16 -0300

    feat: execution .h contracts

--- include/minishell.h
+++ include/minishell.h
@@ -2,5 +2,47 @@
 #define MINISHELL_H
 
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+typedef enum e_type
+{
+    REDIR_IN,     // <
+    REDIR_OUT,    // >
+    REDIR_APPEND, // >>
+    REDIR_HEREDOC // <<
+} t_type;
+
+typedef struct s_redir
+{
+    t_type type;
+    char *file; // nome do arquivo
+    struct s_redir *next;
+} t_redir;
+
+typedef struct s_cmd
+{
+    char **args;           // {"ls", "-la", NULL};
+    t_redir *redirections; // lista de redireções
+    pid_t pid;             // id do processo
+    struct s_cmd *next;    // prox comando do pipe
+} t_cmd;
+
+// exemplo cat < in.txt | grep oi > out.txt
+
+// no 1
+//  args = {"cat", NULL}
+//  redirects =  [type: REDIR_IN, file: "in.txt", next: NULL]
+//  pid = preenchido dps do fork
+//  next = &no2
+
+// no 2
+//  args = {"grep", "oi", NULL}
+//  redirects =  [type: REDIR_OUT, file: "out.txt", next: NULL]
+//  pid = PID DPS DO FORK
+//  next = NULL
 
 #endif
