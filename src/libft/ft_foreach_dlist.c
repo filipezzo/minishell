ef0bc09e75d801980e9abe11109956325e90690a
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-12-09 17:30:49 -0300

    feat: foreach list of

---
+++ src/libft/ft_foreach_dlist.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_foreach_dlist.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 15:47:04 by mhidani           #+#    #+#             */
+/*   Updated: 2025/12/09 17:29:05 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * Iterate throught the double linked list by applying the function to the 
+ * information.
+ * @param list Double linked list used used for the application for each element 
+ * that composes it using the callback function.
+ * @param data Information.
+ * @param callback Execution function for the information passed.
+ * @return True if you managed to go through the entire list and apply the 
+ * procedure, and false if you did not.
+ */
+t_bool	ft_foreach_dlist(t_dlist *list, void *data, foreach_callback callback)
+{
+	t_bnode	*pivot;
+
+	if (!list || !callback)
+		return (FALSE);
+	pivot = list->head;
+	while (pivot)
+	{
+		if (callback(pivot->data, data) == FALSE)
+			return (FALSE);
+		pivot = pivot->next;
+	}
+	return (TRUE);
+}
