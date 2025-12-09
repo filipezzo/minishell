cf694d959b248067451d0f12c686b1febdcf4a58
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-12-09 17:13:06 -0300

    feat: destroy bidirectional node

---
+++ src/libft/ft_destroy_bnode.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_destroy_bnode.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 11:23:37 by mhidani           #+#    #+#             */
+/*   Updated: 2025/12/09 12:11:14 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * Destroy the bidirectional node.
+ * @param ptr Pointer reference to the bidirectional node.
+ * @return True if the destruction process node was successfull, and false if it
+ * was not possible to destroy bidirectional node.
+ */
+t_bool	ft_destroy_bnode(void *ptr)
+{
+	t_bnode	*node;
+
+	if (!ptr || !((t_bnode *)ptr)->destroy_data)
+		return (FALSE);
+	node = (t_bnode *)ptr;
+	node->destroy_data(node->data);
+	free(node);
+	return (TRUE);
+}
