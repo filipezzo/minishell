eae94376fc8492b8062b3472058f7a506bf41422
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-12-28 16:48:49 -0300

    feat: syntax error msg with type

--- src/front/syntax_error_msg.c
+++ src/front/syntax_error_msg.c
@@ -6,13 +6,19 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 18:19:15 by mhidani           #+#    #+#             */
-/*   Updated: 2025/12/27 18:21:01 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/12/28 16:24:55 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_bool	syntax_error_message(char *msg)
+t_bool	syntax_err_msg(char *msg, char *oper)
+{
+	ft_fputstr_fd(STDERR_FILENO, "minishell: %s '%s'\n", msg, oper);
+	return (FALSE);
+}
+
+t_bool	syntax_err_smsg(char *msg)
 {
 	ft_fputstr_fd(STDERR_FILENO, "minishell: %s\n", msg);
 	return (FALSE);
