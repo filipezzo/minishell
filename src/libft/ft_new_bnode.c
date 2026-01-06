a4aef2f24cb5ff54dbe1cfb6e14aa4dc0b04c374
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-06 00:57:49 -0300

    refact: change prototype destroy dt

--- src/libft/ft_new_bnode.c
+++ src/libft/ft_new_bnode.c
@@ -6,21 +6,21 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 09:55:06 by mhidani           #+#    #+#             */
-/*   Updated: 2025/12/13 17:53:09 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/01/06 00:51:18 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /**
- * Creates a node with bidirectional navigation pointers. Initially loose, not 
+ * Creates a node with bidirectional navigation pointers. Initially loose, not
  * directed to any node.
  * @param data Node information.
  * @param structure Reference to the data structure to which the node belongs.
  * @param dst Reference to the function destroy data.
  * @return Memory-allocated node.
  */
-t_bnode	*ft_new_bnode(void *data, void *structure, destructor dst)
+t_bnode	*ft_new_bnode(void *data, void *structure, destroy dst)
 {
 	t_bnode	*node;
 
