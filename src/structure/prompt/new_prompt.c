baf88db55dc4a4db9eaf53967e65093e1f4de13d
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-06 00:59:53 -0300

    feat: new prompt struc

---
+++ src/structure/prompt/new_prompt.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   new_prompt.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/05 20:22:45 by mhidani           #+#    #+#             */
+/*   Updated: 2026/01/05 20:22:54 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_prompt	*new_prompt(void)
+{
+	t_prompt	*prompt;
+
+	prompt = malloc(sizeof(t_prompt));
+	if (!prompt)
+		return (NULL);
+	build_user_pmt(&prompt->user);
+	build_host_pmt(&prompt->host);
+	build_home_pmt(&prompt->home);
+	build_dir_pmt(prompt->home, &prompt->dir);
+	build_type_pmt(prompt->user, &prompt->type);
+	return (prompt);
+}
