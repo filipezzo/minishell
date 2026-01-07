6f05b8aa12f1f18169e584853b9a7e5cb3dfecf5
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-07 17:51:05 -0300

    fix: check valid subshell and exit err

--- src/front/parser/parse_subshell.c
+++ src/front/parser/parse_subshell.c
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 18:23:21 by mhidani           #+#    #+#             */
-/*   Updated: 2026/01/05 18:53:27 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/01/07 17:38:53 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 
 t_tnode	*parse_subshell(t_astree *tree, t_bnode **cursor)
 {
-	t_tnode	*node;
-	t_tnode	*aux;
+	t_tnode		*node;
+	t_tnode		*aux;
+	t_lextoken	*token;
 
 	next_lextoken(cursor);
-	aux = parse_expression(tree, cursor);
+	aux = parse_and_or(tree, cursor);
 	if (!aux)
 		return (NULL);
-	node = new_tnode(tree, ft_strdup("subshell"));
+	token = get_lextoken(*cursor);
+	if (!token || token->type != RIGHT_PAREN)
+		return (destroy_tnode(aux), NULL);
+	node = new_tnode(tree, ft_strdup("SUBSHELL"));
+	if (!node)
+		return (destroy_tnode(aux), NULL);
 	node->type = SUBSHELL;
 	node->left = aux;
 	node->left->branch = node;
