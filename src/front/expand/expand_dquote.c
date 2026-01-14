daf144b639bb21e587a10c2f65eb02d1eeff2eb1
Mauricio Mityo Hidani <mhidani@c1r6p1.42sp.org.br>
2026-01-14 12:53:47 -0300

    test: expand double-quotes

--- src/front/expand/expand_dquote.c
+++ src/front/expand/expand_dquote.c
@@ -6,15 +6,34 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:13:16 by mhidani           #+#    #+#             */
-/*   Updated: 2026/01/13 23:42:28 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/01/14 12:51:53 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	expand_dquote(t_shell *sh, char *src, t_cmd *cmd) // TODO: implement
+static void	join_args(char *src, char **args, size_t idx);
+
+void	expand_dquote(t_shell *sh, t_cmd *cmd, size_t idx)
 {
-	(void)sh;
-	(void)src;
-	(void)cmd;
+	char	*src;
+	char	*value;
+	size_t	i;
+
+	src = ft_strdup(cmd->args[idx]);
+	if (!src)
+		return ;
+	i = 0;
+	while (src[i])
+	{
+		value = find_expand(sh, src, &i);
+		i++;
+	}
+	free(cmd->args[idx]);
+	cmd->args[idx] = src;
 }
+
+// TODO: preciso criar uma forma para conseguir alocar o valor encontrado nas 
+// environments para trocar com sua posição o valor.
+static void	join_args(char *src, char **args, size_t idx)
+{}
