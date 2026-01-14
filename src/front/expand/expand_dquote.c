6cba6a40cef9ef928349faf566a12ab55c4785e9
Mauricio Mityo Hidani <mhidani@c1r6p1.42sp.org.br>
2026-01-14 13:55:23 -0300

    test: expand double-quotes

--- src/front/expand/expand_dquote.c
+++ src/front/expand/expand_dquote.c
@@ -6,17 +6,18 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:13:16 by mhidani           #+#    #+#             */
-/*   Updated: 2026/01/14 12:51:53 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/01/14 13:51:07 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	join_args(char *src, char **args, size_t idx);
+static void	join_args(char *src, char *env, t_cmd *cmd, size_t idx);
 
 void	expand_dquote(t_shell *sh, t_cmd *cmd, size_t idx)
 {
 	char	*src;
+	char	*env;
 	char	*value;
 	size_t	i;
 
@@ -26,7 +27,14 @@ void	expand_dquote(t_shell *sh, t_cmd *cmd, size_t idx)
 	i = 0;
 	while (src[i])
 	{
-		value = find_expand(sh, src, &i);
+		env = find_env(sh, src, &i);
+		if (*env == '$')
+			value = expand_dollar(sh, env);
+		else if (*env == '~')
+			value = expand_tilde();
+		else
+			return ;
+		join_args(value, env, cmd, idx);
 		i++;
 	}
 	free(cmd->args[idx]);
@@ -35,5 +43,6 @@ void	expand_dquote(t_shell *sh, t_cmd *cmd, size_t idx)
 
 // TODO: preciso criar uma forma para conseguir alocar o valor encontrado nas 
 // environments para trocar com sua posição o valor.
-static void	join_args(char *src, char **args, size_t idx)
-{}
+static void	join_args(char *src, char *env, t_cmd *cmd, size_t idx)
+{
+}
