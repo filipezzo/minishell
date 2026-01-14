a352f0bdbd2d991b2fea2c5386e9ef501b748757
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-13 23:46:13 -0300

    test: expand double quotes

---
+++ src/front/expand/expand_dquote.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_dquote.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 17:13:16 by mhidani           #+#    #+#             */
+/*   Updated: 2026/01/13 23:42:28 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	expand_dquote(t_shell *sh, char *src, t_cmd *cmd) // TODO: implement
+{
+	(void)sh;
+	(void)src;
+	(void)cmd;
+}
