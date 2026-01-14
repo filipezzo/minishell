f7553cd5123f35580e0a60d263acb073f40efa7e
Mauricio Mityo Hidani <mhidani@c1r6p1.42sp.org.br>
2026-01-14 12:50:41 -0300

    feat: find expander

--- src/front/expand/handle_expand.c
+++ src/front/expand/handle_expand.c
@@ -6,11 +6,39 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:27:28 by mhidani           #+#    #+#             */
-/*   Updated: 2026/01/14 09:13:51 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/01/14 12:39:55 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static char	*get_dollar_env(char *src, size_t *idx);
+
+char	*find_expand(t_shell *sh, char *src, size_t *idx)
+{
+	char	*env;
+	char	*value;
+	size_t	j;
+
+	while (src[*idx])
+	{
+		if (src[*idx] == '$')
+		{
+			env = get_dollar_env(src, idx);
+			value = expand_dollar(sh, env);
+			free(env);
+			return (value);
+		}
+		else if (src[*idx] == '~')
+		{
+			value = getenv("HOME");
+			*idx++;
+			return (ft_strdup(value));
+		}
+		*idx++;
+	}
+	return (NULL);
+}
 
 char	*expand_dolar(t_shell *sh, char *src)
 {
@@ -47,4 +75,19 @@ char	*expand_tilde(char *src)
 	if (!value)
 		return (NULL);
 	return (value);
+}
+
+static char	*get_dollar_env(char *src, size_t *idx)
+{
+	size_t	j;
+	char	*env;
+
+	if (src[*idx] != '$')
+		return (NULL);
+	j = *idx + 1;
+	while (src[j] && (ft_isalnum(src[j]) || src[j] == '_'))
+		j++;
+	ft_strlcpy(env, (src + *idx), j - *idx - 1);
+	*idx = j;
+	return (env);
 }
