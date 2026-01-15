0cff619fafca883cf992d5fa84703036d8a1bbc1
Filipe Avanzzo <fsousa@student.42sp.org.br>
2026-01-15 13:24:01 -0300

    refactor: heredoc norminette

--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -1,52 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsousa <fsousa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/15 13:16:13 by fsousa            #+#    #+#             */
+/*   Updated: 2026/01/15 13:21:18 by fsousa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "minishell.h"
 
-static int is_delimiter(const char *line, const char *delimiter)
+static int	is_delimiter(const char *line, const char *delimiter)
 {
-    if (!line || !delimiter)
-        return 0;
-    return (ft_strncmp(line, delimiter, ft_strlen(delimiter) + 1) == 0);
+	if (!line || !delimiter)
+		return (0);
+	return (ft_strncmp(line, delimiter, ft_strlen(delimiter) + 1) == 0);
 }
 
-int run_heredoc(char *delimiter)
+static int	handle_signal_status(char *line, int *p)
 {
-    int p[2];
-    char *line;
+	if (g_signal_status == 130)
+	{
+		if (line)
+			free(line);
+		close(p[0]);
+		close(p[1]);
+		return (0);
+	}
+	return (1);
+}
 
-    if (pipe(p) == -1)
-    {
-        perror("pipe heredoc");
-        return (-1);
-    }
+static int	handle_pipe(int *p)
+{
+	if (pipe(p) == -1)
+	{
+		perror("pipe heredoc");
+		return (0);
+	}
+	return (1);
+}
 
-    g_signal_status = 0;
-    set_signals_heredoc();
+int	run_heredoc(char *delimiter)
+{
+	int		p[2];
+	char	*line;
 
-    while (1)
-    {
-        line = readline("> ");
-
-        if (g_signal_status == 130)
-        {
-            if (line)
-                free(line);
-            close(p[0]);
-            close(p[1]);
-            return (-1);
-        }
-
-        if (!line)
-            break;
-        if (is_delimiter(line, delimiter))
-        {
-            free(line);
-            break;
-        }
-        write(p[1], line, ft_strlen(line));
-        write(p[1], "\n", 1);
-        free(line);
-    }
-
-    close(p[1]);
-    return (p[0]);
+	if (!handle_pipe(p))
+		return (-1);
+	g_signal_status = 0;
+	set_signals_heredoc();
+	while (1)
+	{
+		line = readline("> ");
+		if (!handle_signal_status(line, p))
+			return (-1);
+		if (!line)
+			break ;
+		if (is_delimiter(line, delimiter))
+		{
+			free(line);
+			break ;
+		}
+		write(p[1], line, ft_strlen(line));
+		write(p[1], "\n", 1);
+		free(line);
+	}
+	close(p[1]);
+	return (p[0]);
 }
--- src/heredoc/heredoc_fds.c
+++ src/heredoc/heredoc_fds.c
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc_fds.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsousa <fsousa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/15 13:15:34 by fsousa            #+#    #+#             */
+/*   Updated: 2026/01/15 13:15:42 by fsousa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void close_heredoc_fds_cmd(t_cmd *cmd)
+void	close_heredoc_fds_cmd(t_cmd *cmd)
 {
-    t_redir *r;
+	t_redir	*r;
 
-    if (!cmd)
-        return;
-    r = cmd->redirections;
-    while (r)
-    {
-        if (r->type == REDIR_HEREDOC && r->heredoc_fd != -1)
-        {
-            close(r->heredoc_fd);
-            r->heredoc_fd = -1;
-        }
-        r = r->next;
-    }
+	if (!cmd)
+		return ;
+	r = cmd->redirections;
+	while (r)
+	{
+		if (r->type == REDIR_HEREDOC && r->heredoc_fd != -1)
+		{
+			close(r->heredoc_fd);
+			r->heredoc_fd = -1;
+		}
+		r = r->next;
+	}
 }
