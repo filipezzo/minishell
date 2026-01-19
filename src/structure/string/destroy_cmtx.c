5eeaff3efe563c8c90d709d31873073559a28254
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-19 15:01:12 -0300

    refact: rename to destroy char mtx

--- src/structure/string/destroy_cmtx.c
+++ src/structure/string/destroy_cmtx.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   destroy_string_lst.c                              :+:      :+:    :+:   */
+/*   destroy_cmtx.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/05 15:08:17 by mhidani           #+#    #+#             */
-/*   Updated: 2026/01/05 15:09:38 by mhidani          ###   ########.fr       */
+/*   Created: 2026/01/19 08:37:48 by mhidani           #+#    #+#             */
+/*   Updated: 2026/01/19 09:04:00 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	destroy_string_lst(char **list)
+void	destroy_cmtx(char **list)
 {
 	size_t	i;
 
