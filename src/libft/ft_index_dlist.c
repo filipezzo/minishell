edfa4ae4a9403a00f1e37dbc7edbc5f17479920f
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-20 01:44:48 -0300

    feat: indexof dlist

---
+++ src/libft/ft_index_dlist.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_index_dlist.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/19 22:31:00 by mhidani           #+#    #+#             */
+/*   Updated: 2026/01/19 23:16:50 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static t_bnode	*travel_back(t_bnode **node, size_t size, size_t idx);
+static t_bnode	*travel_front(t_bnode **node, size_t size, size_t idx);
+
+t_bnode	*ft_indexof_dlist(t_dlist *list, size_t idx)
+{
+	t_bnode	*node;
+
+	if (!list || idx >= list->size)
+		return (NULL);
+	if (idx > (list->size / 2))
+	{
+		node = list->tail;
+		travel_back(&node, list->size, idx);
+	}
+	else
+	{
+		node = list->head;
+		travel_front(&node, list->size, idx);
+	}
+	return (node);
+}
+
+static t_bnode	*travel_back(t_bnode **node, size_t size, size_t idx)
+{
+	size_t	i;
+
+	i = size - 1;
+	while (i > idx)
+	{
+		i--;
+		*node = (*node)->left;
+	}
+}
+
+static t_bnode	*travel_front(t_bnode **node, size_t size, size_t idx)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < idx)
+	{
+		i++;
+		*node = (*node)->right;
+	}
+}
